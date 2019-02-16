006b301b24214896038308c6c72e4a835b2a80f1
陈俊坚 <36469823+BrendanFich@users.noreply.github.com>
2019-02-16 21:14:39 +0800

    Update b.js

--- sample/js/b.js
+++ sample/js/b.js
@@ -1,5 +1,5 @@
 var xhr = new XMLHttpRequest()
-xhr.open('GET', '/getName', true)
+xhr.open('GET', '/sample/getName', true)
 xhr.send()
 xhr.onload = function(){
 	console.log(JSON.parse(xhr.responseText))
